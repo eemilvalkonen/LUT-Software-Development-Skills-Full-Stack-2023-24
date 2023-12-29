9a40381e5bd5deb84ba26ea813273f13fffc4d0d
eemilvalkonen <valkonen.eemil@hotmail.com>
2023-12-30 00:52:33 +0200

    30-12-2023

---
+++ Members.js
@@ -0,0 +1,19 @@
+const members = [
+    { 
+        id: 1,
+        name: 'Bob Smith', 
+        age: 40 
+    },
+    { 
+        id: 2,
+        name: 'Smith John', 
+        age: 30 
+    },
+    { 
+        id: 3,
+        name: 'John Doe',
+        age: 20 
+    }
+];
+
+module.exports = members;
